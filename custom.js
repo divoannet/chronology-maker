0f2f91860b8e229f3201a8d94d5a28b9eccbdf6c
Анна Федосеева <divoannet@MacBook-Pro-Anna.local>
2019-02-20 22:53:23 +0700

    fix

--- custom.js
+++ custom.js
@@ -1,27 +1,3 @@
 const config = require("./configs").configBuilder;
-
-console.log(config);
-
-// const outputPath = `./data/${config.forumName}.json`;
-// const data = require(outputPath);
-
-// // some code here
-// const olddata = require(`./data/${config.forumName}old.json`);
-
-// const newData = [];
-
-// olddata.forEach(oldtopic => {
-//     const newtopic = data.find(topic => topic.url === oldtopic.url);
-//     if (!newtopic) {
-//         newData.push(oldtopic);
-//         return;
-//     }
-//     const topic = {
-//         ...oldtopic,
-//         ...newtopic
-//     };
-//     topic.categories = oldtopic.categories.split(' ');
-//     newData.push(topic);
-// });
-
-// console.log(newData[100]);
+const outputPath = `./data/${config.forumName}.json`;
+const data = require(outputPath);
