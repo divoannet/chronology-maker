58f575540c52c43cdbd91f542394fe92a4cea4eb
anborgh <divoannet@gmail.com>
2023-08-27 13:44:22 +0300

    config template update

--- configs/config.js
+++ configs/config.js
@@ -3,8 +3,18 @@
  */
 
 module.exports = {
+    "url": "",                      // ссылка на форум
+                                    // не обязательно, если игровые разделы открыты:
+    "login": "login",               // логин аккаунта читателя, с него должны быть видны все игровые и архивные разделы
+    "password": "password",         // пароль аккаунта читателя
+
     "forumName": "fileOfJsonName",
-    "forums": [
+    "formatDate": true,             // нужно ли форматировать даты
+    "systemDateFormat": "YYYY-MM-DD HH:mm:ss", // формат дат, указанный в настройках форума
+    "dateSeparator": "[]",          // как отделяется дата эпизода в названии темы
+    "dateCenture": "20",            // если в дате эпизода используется сокращённая запись даты DD.MM.YY, указать век
+    "updateStatus": "actual",       // только эпизоды в этом статусе будут обновлять участников
+    "forums": [                     // список игровых разделов и меток для них
         {
             url: "",
             id: "completed"
@@ -14,21 +24,22 @@ module.exports = {
             id: "uncompleted"
         },
         {
-            url: "",
-            id: 'remove',
-            remove: true
+            url: "",        // ссылка на раздел
+            id: 'remove',   // статус эпизода в этом разделе
+            remove: true    // нужно ли удалять из хронологии
         }
     ],
-    "additionalFields": [
+    "additionalFields": [           // доп поля таблицы под ручное заполнение
         {
             name: 'description',
             type: 'string',
         },
     ],
     "replace": {
-        "oldCharName": "newCharName"
+        "oldCharName": "newCharName"    // если аккаунт переименован, а персонаж тот же
     },
-    "ignore": [
+    "ignoreUsers": ['Celestia'],        // технические аккаунты
+    "ignore": [                         // темы в разделах с эпизодами, не являющиеся эпизодами (шаблоны, например)
         "http://link_to_ignored_topic"
     ]
 };
